035cb48880fa0cffa4e7d770d050075adbcbeecf
wunnr <wunner@protonmail.com>
2025-09-14 22:19:40 -0600

    New json loading and saving

--- src/handler.rs
+++ src/handler.rs
@@ -1,165 +1,83 @@
 use crate::paths::*;
-use crate::util::*;
+use crate::util::copy_dir_recursive;
 
 use eframe::egui::{self, ImageSource};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-#[derive(Clone)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Handler {
     // Members that are determined by context
+    #[serde(skip)]
     pub path_handler: PathBuf,
+    #[serde(skip)]
     pub img_paths: Vec<PathBuf>,
-
+    #[serde(skip)]
     pub uid: String,
+
     pub name: String,
     pub author: String,
     pub version: String,
     pub info: String,
 
-    pub symlink_dir: bool,
+    pub path_gameroot: String,
     pub runtime: String,
     pub is32bit: bool,
     pub exec: String,
     pub args: String,
-    pub copy_instead_paths: Vec<String>,
     pub remove_paths: Vec<String>,
     pub dll_overrides: Vec<String>,
     pub pause_between_starts: Option<f64>,
 
-    pub path_goldberg: String,
-    pub steam_appid: Option<String>,
-    pub coldclient: bool,
+    pub use_goldberg: bool,
+    pub steam_appid: Option<u32>,
 
     pub game_unique_paths: Vec<String>,
 }
 
 impl Handler {
     pub fn new_from_uid(uid: &str) -> Self {
+        let path_handler = PATH_PARTY.join("handlers").join(uid);
         Self {
-            path_handler: PathBuf::new(),
+            path_handler,
             img_paths: Vec::new(),
-
+            path_gameroot: String::new(),
             uid: uid.to_string(),
+
             name: String::new(),
             author: String::new(),
             version: String::new(),
             info: String::new(),
 
-            symlink_dir: false,
             runtime: String::new(),
             is32bit: false,
             exec: String::new(),
             args: String::new(),
-            copy_instead_paths: Vec::new(),
             remove_paths: Vec::new(),
             dll_overrides: Vec::new(),
             pause_between_starts: None,
 
-            path_goldberg: String::new(),
+            use_goldberg: false,
             steam_appid: None,
-            coldclient: false,
 
             game_unique_paths: Vec::new(),
         }
     }
 
-    pub fn new_from_json(json_path: &PathBuf) -> Result<Self, Box<dyn Error>> {
+    pub fn new_from_json2(json_path: &PathBuf) -> Result<Self, Box<dyn Error>> {
         let file = File::open(json_path)?;
-        let reader = BufReader::new(file);
-        let json: Value = serde_json::from_reader(reader)?;
+        let mut handler = serde_json::from_reader::<_, Handler>(BufReader::new(file))?;
 
-        let mut handler = Self {
-            path_handler: PathBuf::new(),
-            img_paths: Vec::new(),
-
-            uid: json["handler.uid"].as_str().unwrap_or_default().to_string(),
-            name: json["handler.name"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string(),
-            info: json["handler.info"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string(),
-            author: json["handler.author"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string(),
-            version: json["handler.version"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string(),
-
-            symlink_dir: json["game.symlink_dir"].as_bool().unwrap_or_default(),
-            is32bit: json["game.32bit"].as_bool().unwrap_or_default(),
-            runtime: json["game.runtime"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string(),
-            exec: json["game.exec"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string()
-                .sanitize_path(),
-            args: json["game.args"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string()
-                .sanitize_path(),
-            copy_instead_paths: json["game.copy_instead_paths"]
-                .as_array()
-                .map(|arr| {
-                    arr.iter()
-                        .map(|v| v.as_str().unwrap_or_default().to_string().sanitize_path())
-                        .collect()
-                })
-                .unwrap_or_default(),
-            remove_paths: json["game.remove_paths"]
-                .as_array()
-                .map(|arr| {
-                    arr.iter()
-                        .map(|v| v.as_str().unwrap_or_default().to_string().sanitize_path())
-                        .collect()
-                })
-                .unwrap_or_default(),
-            dll_overrides: json["game.dll_overrides"]
-                .as_array()
-                .map(|arr| {
-                    arr.iter()
-                        .map(|v| v.as_str().unwrap_or_default().to_string())
-                        .collect()
-                })
-                .unwrap_or_default(),
-            pause_between_starts: json["game.pause_between_starts"].as_f64(),
-
-            path_goldberg: json["steam.api_path"]
-                .as_str()
-                .unwrap_or_default()
-                .to_string()
-                .sanitize_path(),
-            steam_appid: json["steam.appid"]
-                .as_str()
-                .and_then(|s| Some(s.to_string())),
-            coldclient: json["steam.gb_coldclient"].as_bool().unwrap_or_default(),
-
-            game_unique_paths: json["profiles.game_paths"]
-                .as_array()
-                .map(|arr| {
-                    arr.iter()
-                        .map(|v| v.as_str().unwrap_or_default().to_string().sanitize_path())
-                        .collect()
-                })
-                .unwrap_or_default(),
-        };
-
-        if !handler.uid.chars().all(char::is_alphanumeric) {
-            return Err("uid must be alphanumeric!".into());
-        }
-
+        handler.uid = json_path
+            .parent()
+            .and_then(|p| p.file_name())
+            .and_then(|name| name.to_str())
+            .unwrap_or_default()
+            .to_string();
         handler.path_handler = json_path
             .parent()
             .ok_or_else(|| "Invalid path")?
@@ -173,6 +91,7 @@ impl Handler {
         Self {
             path_handler: PathBuf::new(),
             img_paths: Vec::new(),
+            path_gameroot: String::new(),
 
             uid: "".to_string(),
             name: String::new(),
@@ -180,22 +99,29 @@ impl Handler {
             version: String::new(),
             info: String::new(),
 
-            symlink_dir: false,
             runtime: String::new(),
             is32bit: false,
             exec: exec.to_string(),
             args: args.split_whitespace().map(|s| s.to_string()).collect(),
-            copy_instead_paths: Vec::new(),
             remove_paths: Vec::new(),
             dll_overrides: Vec::new(),
             pause_between_starts: None,
 
-            path_goldberg: String::new(),
+            use_goldberg: false,
             steam_appid: None,
-            coldclient: false,
 
             game_unique_paths: Vec::new(),
         }
+    }
+
+    pub fn save_to_json(&self) -> Result<(), std::io::Error> {
+        if !self.path_handler.exists() {
+            std::fs::create_dir_all(&self.path_handler)?;
+        }
+
+        let json = serde_json::to_string_pretty(self)?;
+        std::fs::write(self.path_handler.join("handler.json"), json)?;
+        Ok(())
     }
 
     pub fn icon(&self) -> ImageSource<'_> {
@@ -247,6 +173,57 @@ impl Handler {
 
         Ok(())
     }
+
+    pub fn locate_steamapi_path(&self) -> Option<PathBuf> {
+        let dllname = match &self.win() {
+            true => match &self.is32bit {
+                true => "steam_api.dll",
+                false => "steam_api64.dll",
+            },
+            false => "libsteam_api.so",
+        };
+
+        if let Ok(rootpath) = self.get_game_rootpath() {
+            let walk_path = walkdir::WalkDir::new(rootpath)
+                .min_depth(1)
+                .follow_links(false);
+
+            for entry in walk_path {
+                if let Ok(entry) = entry
+                    && entry.file_type().is_file()
+                    && let Some(path_str) = entry.path().to_str()
+                    && path_str.ends_with(dllname)
+                {
+                    return Some(entry.path().to_path_buf());
+                }
+            }
+        }
+
+        None
+    }
+
+    pub fn get_game_rootpath(&self) -> Result<String, Box<dyn Error>> {
+        if let Some(appid) = &self.steam_appid
+            && let Some((app, library)) = steamlocate::SteamDir::locate()?
+                .find_app(*appid)
+                .ok()
+                .flatten()
+        {
+            {
+                let path = library.resolve_app_dir(&app);
+                if path.exists() {
+                    let pathstr = path.to_string_lossy().to_string();
+                    return Ok(pathstr);
+                }
+            }
+        }
+
+        if !self.path_gameroot.is_empty() && Path::new(&self.path_gameroot).exists() {
+            return Ok(self.path_gameroot.clone());
+        }
+
+        Err("Game root path not found".into())
+    }
 }
 
 pub fn scan_handlers() -> Vec<Handler> {
@@ -274,7 +251,7 @@ pub fn scan_handlers() -> Vec<Handler> {
         if !json_path.exists() {
             continue;
         }
-        if let Ok(handler) = Handler::new_from_json(&json_path) {
+        if let Ok(handler) = Handler::new_from_json2(&json_path) {
             out.push(handler);
         }
     }
@@ -313,107 +290,107 @@ pub fn install_handler_from_file(file: &PathBuf) -> Result<(), Box<dyn Error>> {
         return Err("uid must be alphanumeric".into());
     }
 
-    copy_dir_recursive(&dir_tmp, &dir_handlers.join(uid), false, true)?;
+    copy_dir_recursive(&dir_tmp, &dir_handlers.join(uid))?;
     std::fs::remove_dir_all(&dir_tmp)?;
 
     Ok(())
 }
 
-pub fn create_symlink_folder(h: &Handler) -> Result<(), Box<dyn Error>> {
-    let path_root = PathBuf::from(get_rootpath_handler(&h)?);
-    let path_sym = PATH_PARTY.join(format!("gamesyms/{}", h.uid));
-    if path_sym.exists() {
-        return Ok(());
-    }
-    std::fs::create_dir_all(path_sym.to_owned())?;
-    copy_dir_recursive(&path_root, &path_sym, true, false)?;
+// pub fn create_symlink_folder(h: &Handler) -> Result<(), Box<dyn Error>> {
+//     let path_root = PathBuf::from(get_rootpath_handler(&h)?);
+//     let path_sym = PATH_PARTY.join(format!("gamesyms/{}", h.uid));
+//     if path_sym.exists() {
+//         return Ok(());
+//     }
+//     std::fs::create_dir_all(path_sym.to_owned())?;
+//     copy_dir_recursive(&path_root, &path_sym, true, false)?;
 
-    // copy_instead_paths takes symlink files and replaces them with their real equivalents
-    for path in &h.copy_instead_paths {
-        let src = path_root.join(path);
-        if !src.exists() {
-            continue;
-        }
-        let dest = path_sym.join(path);
-        println!("src: {}, dest: {}", src.display(), dest.display());
-        if src.is_dir() {
-            println!("Copying directory: {}", src.display());
-            copy_dir_recursive(&src, &dest, false, true)?;
-        } else if src.is_file() {
-            println!("Copying file: {}", src.display());
-            if dest.exists() {
-                std::fs::remove_file(&dest)?;
-            }
-            std::fs::copy(&src, &dest)?;
-        }
-    }
-    for path in h.remove_paths.iter().chain(h.game_unique_paths.iter()) {
-        let p = path_sym.join(path);
-        if !p.exists() {
-            continue;
-        }
-        if p.is_dir() {
-            std::fs::remove_dir_all(p)?;
-        } else if p.is_file() {
-            std::fs::remove_file(p)?;
-        }
-    }
-    let copypath = PathBuf::from(&h.path_handler).join("copy_to_symdir");
-    if copypath.exists() {
-        copy_dir_recursive(&copypath, &path_sym, false, true)?;
-    }
+//     // copy_instead_paths takes symlink files and replaces them with their real equivalents
+//     for path in &h.copy_instead_paths {
+//         let src = path_root.join(path);
+//         if !src.exists() {
+//             continue;
+//         }
+//         let dest = path_sym.join(path);
+//         println!("src: {}, dest: {}", src.display(), dest.display());
+//         if src.is_dir() {
+//             println!("Copying directory: {}", src.display());
+//             copy_dir_recursive(&src, &dest, false, true)?;
+//         } else if src.is_file() {
+//             println!("Copying file: {}", src.display());
+//             if dest.exists() {
+//                 std::fs::remove_file(&dest)?;
+//             }
+//             std::fs::copy(&src, &dest)?;
+//         }
+//     }
+//     for path in h.remove_paths.iter().chain(h.game_unique_paths.iter()) {
+//         let p = path_sym.join(path);
+//         if !p.exists() {
+//             continue;
+//         }
+//         if p.is_dir() {
+//             std::fs::remove_dir_all(p)?;
+//         } else if p.is_file() {
+//             std::fs::remove_file(p)?;
+//         }
+//     }
+//     let copypath = PathBuf::from(&h.path_handler).join("copy_to_symdir");
+//     if copypath.exists() {
+//         copy_dir_recursive(&copypath, &path_sym, false, true)?;
+//     }
 
-    // Insert goldberg dll
-    if !h.path_goldberg.is_empty() {
-        let dest = path_sym.join(&h.path_goldberg);
+//     // Insert goldberg dll
+//     if !h.path_goldberg.is_empty() {
+//         let dest = path_sym.join(&h.path_goldberg);
 
-        let steam_settings = dest.join("steam_settings");
-        if !steam_settings.exists() {
-            std::fs::create_dir_all(steam_settings.clone())?;
-        }
-        if let Some(appid) = &h.steam_appid {
-            std::fs::write(steam_settings.join("steam_appid.txt"), appid.as_str())?;
-        }
+//         let steam_settings = dest.join("steam_settings");
+//         if !steam_settings.exists() {
+//             std::fs::create_dir_all(steam_settings.clone())?;
+//         }
+//         if let Some(appid) = &h.steam_appid {
+//             std::fs::write(steam_settings.join("steam_appid.txt"), appid.as_str())?;
+//         }
 
-        // If the game uses goldberg coldclient, assume the handler owner has set up coldclient in the copy_to_symdir files
-        // And so we don't copy goldberg dlls or generate interfaces
-        if h.coldclient {
-            return Ok(());
-        }
+//         // If the game uses goldberg coldclient, assume the handler owner has set up coldclient in the copy_to_symdir files
+//         // And so we don't copy goldberg dlls or generate interfaces
+//         if h.coldclient {
+//             return Ok(());
+//         }
 
-        let mut src = PATH_RES.clone();
-        src = match &h.win() {
-            true => src.join("goldberg/win"),
-            false => src.join("goldberg/linux"),
-        };
-        src = match &h.is32bit {
-            true => src.join("x32"),
-            false => src.join("x64"),
-        };
+//         let mut src = PATH_RES.clone();
+//         src = match &h.win() {
+//             true => src.join("goldberg/win"),
+//             false => src.join("goldberg/linux"),
+//         };
+//         src = match &h.is32bit {
+//             true => src.join("x32"),
+//             false => src.join("x64"),
+//         };
 
-        copy_dir_recursive(&src, &dest, false, true)?;
+//         copy_dir_recursive(&src, &dest, false, true)?;
 
-        let path_steamdll = path_root.join(&h.path_goldberg);
-        let steamdll = match &h.win() {
-            true => match &h.is32bit {
-                true => path_steamdll.join("steam_api.dll"),
-                false => path_steamdll.join("steam_api64.dll"),
-            },
-            false => path_steamdll.join("libsteam_api.so"),
-        };
+//         let path_steamdll = path_root.join(&h.path_goldberg);
+//         let steamdll = match &h.win() {
+//             true => match &h.is32bit {
+//                 true => path_steamdll.join("steam_api.dll"),
+//                 false => path_steamdll.join("steam_api64.dll"),
+//             },
+//             false => path_steamdll.join("libsteam_api.so"),
+//         };
 
-        let gen_interfaces = match &h.is32bit {
-            true => PATH_RES.join("goldberg/generate_interfaces_x32"),
-            false => PATH_RES.join("goldberg/generate_interfaces_x64"),
-        };
-        let status = std::process::Command::new(gen_interfaces)
-            .arg(steamdll)
-            .current_dir(steam_settings)
-            .status()?;
-        if !status.success() {
-            return Err("Generate interfaces failed".into());
-        }
-    }
+//         let gen_interfaces = match &h.is32bit {
+//             true => PATH_RES.join("goldberg/generate_interfaces_x32"),
+//             false => PATH_RES.join("goldberg/generate_interfaces_x64"),
+//         };
+//         let status = std::process::Command::new(gen_interfaces)
+//             .arg(steamdll)
+//             .current_dir(steam_settings)
+//             .status()?;
+//         if !status.success() {
+//             return Err("Generate interfaces failed".into());
+//         }
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
