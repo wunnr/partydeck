0dfa927d6f7112907a3a6f1db5e634ef7ce61cd1
wunnr <wunner@protonmail.com>
2025-11-02 13:23:17 -0700

    Remove SDL driver println as it's always x11 right now

--- src/monitor.rs
+++ src/monitor.rs
@@ -22,8 +22,6 @@ impl Monitor {
 // Using sdl seems to be the most reliable way to get monitor info in a way that lines up with what gamescope expects, since its --display-index option uses sdl.
 pub fn get_monitors_sdl() -> Vec<Monitor> {
     let video = sdl2::init().unwrap().video().unwrap();
-    println!("[partydeck] SDL driver: {}", video.current_video_driver());
-
     let count = video.num_video_displays().unwrap();
     let mut monitors = Vec::new();
     for i in 0..count {
